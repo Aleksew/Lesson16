8761a45245ca3303bc5be06385c4627c2ee667b8
Aleksew <aleksewww@gmail.com>
2021-08-03 19:45:37 +0300

    Home Work comlite

---
+++ HW16/HW_Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HW_Task1
+{
+    class Program
+    {
+        //Создайте структуру Point.В теле структуры создайте два интовых поля – x и y.
+        //Создайте структуру Vektor.В теле структуры создайте два поля типа Point с именами start и finish.
+        //В теле Vektor выполните перегрузку оператора + таким образом, чтобы в результате сложения двух экземпляров типа Vektor получался новый объект Vektor с значениями координат Point 
+        //в соответствии с правилами сложения векторов:
+        //Выполните такое задание и для разности двух векторов.Выполните перегрузку оператора * для получения скалярного произведения векторов.
+
+        static void Main(string[] args)
+        {
+            var vektor1 = new Vektor(new Point(1,-3), new Point(4,5));
+            var vektor2 = new Vektor(new Point(-2, -1), new Point(3, 0));
+
+            var vektorAddRezult = vektor1 + vektor2;
+            var vektorSubRezult = vektor1 - vektor2;
+
+            var vektorScalarMulRezult = vektor1 * vektor2;
+
+            Console.WriteLine($"Вектор 1:A({vektor1.start.x};{vektor1.start.y}) B({vektor1.finish.x};{vektor1.finish.y})");
+            Console.WriteLine($"Вектор 2:C({vektor2.start.x};{vektor2.start.y}) D({vektor2.finish.x};{vektor2.finish.y})");
+
+            Console.WriteLine("Сложение векторов");
+            Console.WriteLine($"A({vektorAddRezult.start.x};{vektorAddRezult.start.y}) D({vektorAddRezult.finish.x};{vektorAddRezult.finish.y})");
+
+            Console.WriteLine("Вычитание векторов");
+            Console.WriteLine($"A({vektorSubRezult.start.x};{vektorSubRezult.start.y}) D({vektorSubRezult.finish.x};{vektorSubRezult.finish.y})");
+
+            Console.WriteLine("Скалярное произведение векторов");
+            Console.WriteLine(vektorScalarMulRezult);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HW16/HW_Task1/Vektor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Task1
+{
+    public struct Vektor
+    {
+        public Point start;
+        public Point finish;
+
+        public Vektor(Point start, Point finish)
+        {
+            this.start = start;
+            this.finish = finish;
+        }
+
+        public static Vektor operator +(Vektor v1, Vektor v2)
+        {
+            int x1 = v1.finish.x - v1.start.x;
+            int y1 = v1.finish.y - v1.start.y;
+
+            int x2 = v1.finish.x + (v2.finish.x - v2.start.x);
+            int y2 = v1.finish.y + (v2.finish.y - v2.start.y);
+
+            return new Vektor(new Point(v1.start.x, v1.start.y), new Point(x2, y2));
+        }
+        public static Vektor operator -(Vektor v1, Vektor v2)
+        {
+            int x1 = v1.finish.x + v1.start.x;
+            int y1 = v1.finish.y + v1.start.y;
+
+            int x2 = v1.finish.x - (v2.finish.x - v2.start.x);
+            int y2 = v1.finish.y - (v2.finish.y - v2.start.y);
+
+            return new Vektor(new Point(v1.start.x, v1.start.y), new Point(x2, y2));
+        }
+        public static double operator *(Vektor v1, Vektor v2)
+        {
+            int x1 = v1.finish.x - v1.start.x;
+            int y1 = v1.finish.y - v1.start.y;
+
+            int x2 = v2.finish.x - v2.start.x;
+            int y2 = v2.finish.y - v2.start.y;
+
+            return (x1 * x2) + (y1 * y2);
+        }
+    }
+}
